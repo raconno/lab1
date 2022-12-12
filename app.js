5d6bcb69eece20a95d1b3af74e9712ddb84c3c41
Ira2753 <repkadrakon@gmail.com>
2022-12-12 12:03:37 +0200

    break tabs and ;

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => res.send("Who are U?"))
+        app.get('/', (req, res) => res.send('Who are U?'));
 app.listen(80, () => console.log('listening on port 80'));
